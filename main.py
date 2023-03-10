4241a3a7a1f58976cfc73e2c7878e0101373f967
caesarnetyet <caesarnetyet@gmail.com>
2023-03-10 12:07:04 -0600

    test

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from controllers.arduino_controller import ArduinoController
 from controllers.arduino_controller.sensor import Sensor
+from temperatura import Temperatura
 
 distancia = Sensor(5, 6, tipo="dis")
 sonido = Sensor(25, tipo="son")
 temp_hum = Sensor(pin_in=4, tipo="temp")
+
+temperatura = Temperatura()
+while True:
+    print(temperatura.get_temperatura_humedad())
 
 arduino = ArduinoController("dustbinv1", 1)
 
---
+++ temperatura.py
@@ -0,0 +1,18 @@
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+
+
+class Temperatura:
+    def __init__(self):
+        self.sensor = Adafruit_DHT.DHT11
+        self.pin = 4
+        GPIO.setmode(GPIO.BCM)
+
+    def get_temperatura_humedad(self):
+        temperatura, humedad = Adafruit_DHT.read(self.sensor, self.pin)
+
+        if temperatura is not None and humedad is not None:
+            datos = [temperatura, humedad]
+            return datos
+        else:
+            return [0, 0]
