687c6da8721d20ef912b41f67bedee6ff56dc18b
caesarnetyet <caesarnetyet@gmail.com>
2023-03-10 11:30:36 -0600

    jala porfa

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from controllers.arduino_controller import ArduinoController
 from controllers.arduino_controller.sensor import Sensor
 
-sensors = Sensor(5, 6, tipo="dis")
+distancia = Sensor(5, 6, tipo="dis")
+sonido = Sensor(25, tipo="son")
+temp_hum = Sensor(21, tipo="temp")
 
 arduino = ArduinoController("dustbinv1", 1)
 
-arduino.add_arduino(sensors)
+arduino.add_arduino(distancia)
+arduino.add_arduino(sonido)
+arduino.add_arduino(temp_hum)
 
 arduino.export_arduino_data()
