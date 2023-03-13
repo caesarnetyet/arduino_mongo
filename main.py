f8ff6785cc0f9a4f606948b9db3d40edb6ab3851
caesarnetyet <caesarnetyet@gmail.com>
2023-03-13 10:09:01 -0600

    brick

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from controllers.arduino_controller import ArduinoController
 from controllers.arduino_controller.sensor import Sensor
-from temperatura import Temperatura
 
-distancia = Sensor(5, 6, tipo="dis", description="Sensor de distancia en tapa")
+distancia = Sensor(6, 5, tipo="dis", description="Sensor de distancia en tapa")
 sonido = Sensor(25, tipo="son", description="Sensor de sonido hipotetico")
-temp_hum = Sensor(pin_in=4, tipo="temp", description="Detector de humedad para el bote")
-led = Sensor(pin_out=26, tipo="led", description="Led de actividad")
+temp_hum = Sensor(pin_in=24, tipo="temp", description="Detector de humedad para el bote")
+led = Sensor(pin_out=4, tipo="led", description="Led de actividad")
 
 arduino = ArduinoController("dustbinv1", 1)
 
