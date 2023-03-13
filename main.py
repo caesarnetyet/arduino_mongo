7298ccbd37d48f68aeb17e8a25ed30c091dbf557
caesarnetyet <caesarnetyet@gmail.com>
2023-03-13 11:12:10 -0600

    yaa porfa

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from controllers.arduino_controller.sensor import Sensor
 
 distancia = Sensor(6, 5, tipo="dis", id_="dis1",description="Sensor de distancia en tapa")
 sonido = Sensor(25, tipo="son", id_="son1", description="Sensor de sonido hipotetico")
-temp = Sensor(pin_in=17, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
-hum = Sensor(pin_in=17, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
+temp = Sensor(pin_in=21, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
+hum = Sensor(pin_in=21, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
 led = Sensor(pin_out=4, tipo="led", id_="led1", description="Led de actividad")
+
+
 arduino = ArduinoController("dustbinv1", 1)
 
 arduino.add_arduino(distancia)
