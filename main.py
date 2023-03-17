881591e88922643a64c57a55f93c28b6069b663d
caesarnetyet <caesarnetyet@gmail.com>
2023-03-17 10:57:12 -0600

    pushing

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from controllers.arduino_controller import ArduinoController
 from controllers.arduino_controller.sensor import Sensor
 
-distancia = Sensor(6, 5, tipo="dis", id_="dis1",description="Sensor de distancia en tapa")
-sonido = Sensor(25, tipo="son", id_="son1", description="Sensor de sonido hipotetico")
-temp = Sensor(pin_in=21, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
-hum = Sensor(pin_in=21, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
-led = Sensor(pin_out=4, tipo="led", id_="led1", description="Led de actividad")
+distancia = Sensor(23, 24, tipo="dis", id_="dis1",description="Sensor de distancia en tapa")
+sonido = Sensor(6, tipo="son", id_="son1", description="Sensor de sonido hipotetico")
+temp = Sensor(pin_in=20, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
+hum = Sensor(pin_in=20, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
+# led = Sensor(pin_out=4, tipo="led", id_="led1", description="Led de actividad")
 
 arduino = ArduinoController("dustbinv1", 15)
 
@@ -13,6 +13,6 @@ arduino.add_arduino(distancia)
 arduino.add_arduino(sonido)
 arduino.add_arduino(temp)
 arduino.add_arduino(hum)
-arduino.add_arduino(led)
+# arduino.add_arduino(led)
 
 arduino.export_arduino_data()
