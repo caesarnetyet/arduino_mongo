343cb01a9887ce6d6764698b15e4b8084745792d
Abelardo <abelardoreyes256@gmail.com>
2023-03-17 22:43:05 -0600

    cambios extraños2

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from controllers.arduino_controller.sensor import Sensor
 
 distancia = Sensor(5, 6, tipo="dis", id_="dis1",description="Sensor de distancia en tapa")
 sonido = Sensor(26, tipo="son", id_="son1", description="Sensor de sonido hipotetico")
-temp = Sensor(pin=2, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
-hum = Sensor(pin=21, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
+temp = Sensor(pin_in=21, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
+hum = Sensor(pin_in=21, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
 # led = Sensor(pin_out=4, tipo="led", id_="led1", description="Led de actividad")
 
 arduino = ArduinoController("dustbinv1", 15)
