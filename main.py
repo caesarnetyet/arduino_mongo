c07dbdf26e9f418df30a9f9b2d95592a847b7ab1
Abelardo <abelardoreyes256@gmail.com>
2023-03-17 22:34:20 -0600

    se cambio la funcion de temperatura y humedad

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from Adafruit import Adafruit
 from controllers.arduino_controller import ArduinoController
 from controllers.arduino_controller.sensor import Sensor
 
-distancia = Sensor(23, 24, tipo="dis", id_="dis1",description="Sensor de distancia en tapa")
-sonido = Sensor(6, tipo="son", id_="son1", description="Sensor de sonido hipotetico")
-temp = Adafruit(pin=20, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
-hum = Adafruit(pin=20, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
+distancia = Sensor(5, 6, tipo="dis", id_="dis1",description="Sensor de distancia en tapa")
+sonido = Sensor(26, tipo="son", id_="son1", description="Sensor de sonido hipotetico")
+temp = Adafruit(pin=2, tipo="temp",  id_="temp1", description="Detector de humedad para el bote")
+hum = Adafruit(pin=21, tipo="hum",  id_="hum1", description="Detector de humedad para el bote")
 # led = Sensor(pin_out=4, tipo="led", id_="led1", description="Led de actividad")
 
 arduino = ArduinoController("dustbinv1", 15)
